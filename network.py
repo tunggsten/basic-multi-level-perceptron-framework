1b96c9517ac2cfabf1f41b66db2361f64c0c6344
tungsten <65860694+tunggsten@users.noreply.github.com>
2024-11-24 15:26:37 +0000

    Made first prototype of backpropagation code; can't test it rn cause vscode refuses to let me run anything

--- network.py
+++ network.py
@@ -38,6 +38,8 @@ class Neuron:
     def export_parameters(self):
         return(f"{self.weights}\\{self.bias}\n")
 
+    def get_derivatives(self):
+        return f"Derivative of cost with respect to weights: \n{self.dCostByDWeights}\nDerivative of cost with respect to bias: \n{self.dCostByDBias}\nDerivative of cost with respect to activation:\n{self.dCostByDActivation}"
 
 
     def calculate_activation(self, inputs:list[float]):
@@ -55,9 +57,6 @@ class Neuron:
     def get_d_weighted_input_by_d_weight(self, weightIndex:int):
         if self.previousLayer:
             return self.previousLayer.neurons[weightIndex].activation
-
-    def get_d_weighted_input_by_d_bias(self):
-        return 1
     
     def get_d_activation_by_d_weighted_input(self):
         return 1 if self.weightedInput >= 0 else LRELULEAKSLOPE
@@ -65,18 +64,32 @@ class Neuron:
 
 
     def calculate_derivatives(self):
-        self.dCostByDActivation = 0
         self.dCostByDWeights = []
         self.dCostByDBias = 0
-
+        
+        dActivationByDInput = neuron.get_d_activation_by_d_weighted_input()
+        
+        # Find d activation by d cost
+        # This is helpful because it lets us collapse our derivative chain for each layer of the network,
+        # so we can process the preceeding layers without redoing big calculations
+        
         if self.nextLayer:
+            self.dCostByDActivation = 0
+            
             for neuron in self.nextLayer:
-                self.dCostByDActivation += neuron.weights[self.neuronIndex] * neuron.get_d_activation_by_d_weighted_input() * neuron.dCostByDActivation 
+                self.dCostByDActivation += (neuron.weights[self.neuronIndex] * 
+                                            dActivationByDInput * 
+                                            neuron.dCostByDActivation)
 
         if self.previousLayer:
+            # Find d cost by d weight for all weights
             for i in range(self.previousLayer.size):
-                self.dCostByDWeights.append(self.dCostByDActivation * self.previousLayer.neurons[i].activation)
-        pass
+                self.dCostByDWeights.append(self.previousLayer.neurons[i].activation *
+                                            dActivationByDInput *
+                                            self.dCostByDActivation)
+                
+            # d weighted input by d bias is always 1, so we don't have to multiply anything here
+            self.dCostByDBias = self.get_d_activation_by_d_weighted_input() * self.dCostByDActivation
 
 
 
@@ -122,6 +135,16 @@ class Layer:
             activations.append(neuron.activation)
 
         return activations
+    
+    def get_layer_derivatives(self):
+        derivatives = []
+        
+        for neuron in self.neurons:
+            derivatives.append(neuron.get_derivatives())
+            
+        return derivatives
+    
+    
 
     def calculate_model_outputs(self, modelInputs:list[float]):
         if self.previousLayer:
@@ -136,6 +159,13 @@ class Layer:
 
             for i in range(len(modelInputs)):
                 self.neurons[i].activation = modelInputs[i]
+                
+    def backpropagate(self):
+        for neuron in self.neurons:
+            neuron.calculate_derivatives()
+            
+        if self.previousLayer:
+            self.previousLayer.backpropagate()
 
 
 
@@ -192,12 +222,22 @@ class Network:
 
         return activations
     
+    def get_network_derivatives(self):
+        derivatives = []
+        
+        for layer in self.layers:
+            derivatives.append(layer.get_layer_derivatives())
+            
+        return derivatives
+    
+    
 
     def get_output(self):
         return self.outputLayer.get_layer_activation()
 
     def generate_output(self, inputs:list[float]):
         self.outputLayer.calculate_model_outputs(inputs)
+
 
     
     def get_ssr(self, expected:list[float]):
@@ -209,6 +249,20 @@ class Network:
             SSR += (outputs[i] - expected[i]) ** 2
 
         return SSR
+    
+    
+    
+    def backpropagate(self, expected:list[float]):
+        # This finds the derivatives of each parameter with respect to the cost when the network's 
+        # current activations are expected to return the expected values
+        
+        for i in range(self.outputLayer.size):
+            outputNeurons = self.outputLayer.neurons
+            outputNeurons[i].dCostByDActivation = -2 * (expected[i] - outputNeurons[i].activation)
+            
+        self.outputLayer.backpropagate()
+        
+        
 
 
 
@@ -270,4 +324,9 @@ print(testNetwork.outputLayer.get_layer_activation())
 
 print(timeMessage)
 
-print(testNetwork.get_ssr([0, 0, 1, 0, 0, 0, 0, 0, 0, 0]))
+print("Starting backpropagation...")
+startTime = time.time()
+testNetwork.backpropagate([0, 0, 1, 0, 0, 0, 0, 0, 0, 0])
+print(f"Finished backpropagation in {time.time() - startTime} seconds.")
+
+print(testNetwork.get_network_derivatives([0, 0, 1, 0, 0, 0, 0, 0, 0, 0]))
